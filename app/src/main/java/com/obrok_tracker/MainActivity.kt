243e42c75bb00e04aa184e784cd72c484f98f766
Patadass <ajdinskiivan1@gmail.com>
2024-04-24 18:01:09 +0200

    changed transition animation from MainActivity to SubActivity

--- app/src/main/java/com/obrok_tracker/MainActivity.kt
+++ app/src/main/java/com/obrok_tracker/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.obrok_tracker
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.transition.Explode
 import android.view.View
-import android.view.Window
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
--- app/src/main/java/com/obrok_tracker/SubActivity.kt
+++ app/src/main/java/com/obrok_tracker/SubActivity.kt
@@ -2,12 +2,10 @@ package com.obrok_tracker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
-import android.window.OnBackInvokedDispatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
